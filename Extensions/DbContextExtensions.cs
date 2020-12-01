a044ecc332467e802d6ed77b52eec7164888af19
Can Acar <can.acar@windowslive.com>
2020-12-01 14:15:41 +0300

    Update DbContextExtensions.cs

--- Extensions/DbContextExtensions.cs
+++ Extensions/DbContextExtensions.cs
@@ -14,6 +14,35 @@ namespace Core.Data.Helper.Extensions
 #pragma warning disable CS8603
     public static class DbContextExtensions
     {
+        public static void RegisterContext<TContext>(this ContainerBuilder builder, string connectionStringName) where TContext : DbContext
+        {
+            builder.Register(componentContext =>
+                {
+                    var ServiceProvider = componentContext.Resolve<IServiceProvider>();
+                    var Configuration = componentContext.Resolve<IConfiguration>();
+                    var DbContextOptions = new DbContextOptions<TContext>(new Dictionary<Type, IDbContextOptionsExtension>());
+                    var OptionsBuilder = new DbContextOptionsBuilder<TContext>(DbContextOptions)
+                        .UseApplicationServiceProvider(ServiceProvider)
+                        .UseSqlServer(Configuration.GetConnectionString(connectionStringName))
+                        // .UseSqlServer(Configuration.GetConnectionString(connectionStringName),
+                        //               serverOptions => serverOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(30), null))
+                        .ConfigureWarnings(c => c.Log((RelationalEventId.CommandExecuting, LogLevel.Debug)));
+
+                    return OptionsBuilder.Options;
+                }).As<DbContextOptions<TContext>>()
+                .InstancePerLifetimeScope();
+
+            builder.Register(context => context.Resolve<DbContextOptions<TContext>>())
+                .As<DbContextOptions>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<TContext>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+            
+            builder.RegisterType<UnitOfWork<TContext>>().As<IUnitOfWork<TContext>>().As<IUnitOfWork>().InstancePerLifetimeScope();
+        }
+
         public static IServiceCollection RegisterContext<TContext>(this IServiceCollection services, string connectionStringName) where TContext : DbContext
         {
             var serviceProvider = services.BuildServiceProvider();
